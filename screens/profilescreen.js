f146ee61f7f7e509e5386872a948725c9d5326c5
justin head <justin_head@ymail.com>
2018-05-29 16:47:18 -0500

    Profile screen is working, but needs score component

--- screens/profilescreen.js
+++ screens/profilescreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { StyleSheet, View, Image } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, H1 } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, H1, Segment } from 'native-base';
 import * as usersService from '../services/users';
 
 
@@ -28,24 +28,34 @@ export default class ProfileScreen extends Component {
         let playerInfo = this.state.player;
         return (
             <Container>
-                <Content contentContainerStyle={{ flex: 1,
-                                                  flexDirection: 'row',
-                                                  justifyContent: 'space-between'}}>
-                    
-                        <Button style={{ alignSelf: 'flex-start' }} onPress={() => this.props.navigation.navigate('Settings', { playerInfo })}>
-                        <Icon name="ios-settings-outline" />
-                        </Button>
-                        <H1>Profile</H1>
-                        <Button style={{ alignSelf: 'flex-start' }} onPress={() => this.props.navigation.navigate('SignedOut')}>
+                <Header>
+                        <Left>
+                            <Button onPress={() => this.props.navigation.navigate('Settings', { playerInfo })}>
+                            <Icon name="ios-settings-outline" />
+                            </Button>
+                        </Left>
+                        <Body>
+                            <Title style={{ paddingLeft: 100 }}>Profile</Title>
+                        </Body>
+                        <Right>
+                            <Button onPress={() => this.props.navigation.navigate('SignedOut')}>
                             <Text>Log Out</Text>
-                        </Button>
-                </Content>
-                <Content contentContainerStyle={{ flex: 1,
-                                                  flexDirection: 'column',
-                                                  justifyContent: 'flex-start',
-                                                  alignItems: 'center'}}>
+                            </Button>
+                        </Right>
+                </Header>
+                <Content contentContainerStyle={{   flex: 0,
+                                                    flexDirection: 'column',
+                                                    justifyContent: 'flex-start',
+                                                    alignItems: 'center',
+                                                    paddingTop: 50}}>
                         
-                        <View><Image source={require('../images/bird.jpg')} style={{height: 100, width: 100, flex: 0}}/>
+                        <View><Image source={require('../images/bird.jpg')} style={{height: 100, width: 100, borderRadius: 50, flex: 0}}/>
+                        </View>
+                        
+                        <View>
+                            <Text style={styles.bold}>User Name: <Text>{this.state.player.username}</Text></Text>
+                            <Text style={styles.bold}>Name: <Text>{this.state.player.name}</Text></Text>
+                            <Text style={styles.bold}>Email: <Text>{this.state.player.email}</Text></Text>
                         </View>
 
                 
@@ -58,8 +68,18 @@ export default class ProfileScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-    photo: {
-      alignSelf: "center",
-      justifyContent: "center"
+    bold: {
+        fontWeight: '700',
+        fontSize: 24
+    },
+    profilePhoto: {
+        borderRadius: 50
+    },
+    viewStyleContainer: {
+        flex: 0,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
     }
   });

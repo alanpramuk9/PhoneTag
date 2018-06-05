8e190e5218aa1921166fa7ff1142aad80d2b5cea
Robert Tate <robertjosephtate@gmail.com>
2018-06-05 09:41:26 -0500

    styled profile screen

--- screens/splashscreen.js
+++ screens/splashscreen.js
@@ -1,37 +1,59 @@
 import React, { Component } from 'react';
 
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-import { AppRegistry, View, Image } from 'react-native';
+import { AppRegistry, View, Image, StyleSheet } from 'react-native';
 
 export default class SplashScreen extends Component {
 
-    // static navigationOptions = {
-    //     header: "none"
-    // }
+    static navigationOptions = {
+        header: null
+    }
 
     render() {
         return (
             <Container>
-                <Content contentContainerStyle={{ justifyContent: 'space-around', alignItems: 'center', flex: 1 }}>
-                    <Text>Jelly Drop</Text>
+                <Content contentContainerStyle={{ justifyContent: 'space-around', alignItems: 'center', flex: 1, backgroundColor: '#81BCFF' }}>
                     <Image
-                        source={require('../images/jellybean.png')}
-                        />
-                    <Button style={{ alignSelf: 'center' }} onPress={() => this.props.navigation.navigate('SignIn')}>
-                        <Text>Sign In</Text>
+                        source={require('../images/jellybean2.png')}
+                    />
+                    
+                    <Button style={styles.button} onPress={() => this.props.navigation.navigate('SignIn')}>
+                        <Text style={{ color: 'black', fontSize: 30, fontFamily: 'EvilIcons'}}>Sign In</Text>
                     </Button>
-                    <Button style={{ alignSelf: 'center' }} onPress={() => this.props.navigation.navigate('SignUp')}>
-                        <Text>Sign Up</Text>
+                    <Button style={styles.button} onPress={() => this.props.navigation.navigate('SignUp')}>
+                        <Text style={{ color: 'black', fontSize: 30}}>Sign Up</Text>
                     </Button>
                 </Content>
-                <Footer>
-                    <Button style={{ alignSelf: 'flex-end' }} onPress={() => this.props.navigation.navigate('About')}>
-                            <Text>Aboout Phone Tag!</Text>
+                <Footer style={styles.footer}>
+                    <Button style={{ backgroundColor: '#464C8A', alignSelf: 'flex-end'}} onPress={() => this.props.navigation.navigate('About')}>
+                        <Text style={{ fontSize: 15, fontWeight: '600'}}>About Phone Tag</Text>
                     </Button>
                 </Footer>
             </Container>
         )
     }
-
-
 }
+
+const styles = StyleSheet.create({
+
+    button: {
+        paddingLeft: 40,
+        paddingRight: 40,
+        paddingTop: 30,
+        paddingBottom: 30,
+        alignSelf: 'center',
+        backgroundColor: '#00FF72',
+        borderColor: 'black',
+        borderRadius: 25,
+        borderWidth: 10,
+        
+    },
+    title: {
+        fontSize: 50,
+    },
+    footer: {
+        backgroundColor: '#464C8A'
+    }
+
+
+})

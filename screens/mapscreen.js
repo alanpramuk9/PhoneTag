562d5536fec43653cd5c5b5b713b39aa78b1d9f0
justin head <justin_head@ymail.com>
2018-06-12 15:36:37 -0500

    change to styles on map

--- screens/mapscreen.js
+++ screens/mapscreen.js
@@ -354,7 +354,6 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginTop: 15,
         marginRight: 150,
-        marginRight: 150,
         borderRadius: 3
     },
     scorePic: {
